8ab584a8bf75640436e7615807f20314e7b019c2
1020017 <remy_b-f@hotmail.com>
2018-01-29 09:15:23 -0500

    Début classe CV et constructeur

--- TP1-AQDL/src/CV.java
+++ TP1-AQDL/src/CV.java
@@ -1,4 +1,21 @@
 
 public class CV {
 
+	private String nom;
+	private String prenom;
+	private String formation;
+	private int exp;
+	private String[] comp;
+	private String attentes;
+	
+	public void CV(String nom, String prenom, String formation, int exp, String[] comp, String attentes) {
+		this.nom = nom;
+		this.prenom = prenom;
+		this.formation = formation;
+		this.exp = exp;
+		this.comp = comp;
+		this.attentes = attentes;
+		
+	}
+	
 }
