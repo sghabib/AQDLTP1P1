7e6dd10517c100f546dfbd21195b33109c42a8a8
Frederik sylvain <frederik.sylvain6@hotmail.com>
2018-02-13 18:05:00 -0500

    Travail Terminer (Pour l'instant)

--- TP2-AQDL/src/Main.java
+++ TP2-AQDL/src/Main.java
@@ -10,14 +10,23 @@ public class Main {
 	
 	
 	public static void main(String[] args) throws IOException {
-		boolean test;
+		int test;
 		String[] texte;
 		texte = Lecture.lectureFichier();
 		tabClient = new NomClient[Integer.parseInt(texte[texte.length-3])];
+		for(int i=0;i<tabClient.length;i++){
+			tabClient[i]=new NomClient();
+		}
 		tabPlat = new Plats[Integer.parseInt(texte[texte.length-2])];
+		for(int i=0;i<tabPlat.length;i++){
+			tabPlat[i]=new Plats();
+		}
 		tabCommande = new Commande[Integer.parseInt(texte[texte.length-1])];
+		for(int i=0;i<tabCommande.length;i++){
+			tabCommande[i]=new Commande();
+		}
 		test=Verifier(texte);
-		if(test){
+		if(test == tabCommande.length){
 			afficherFacture();
 		}else{
 			System.out.println(erreur);
@@ -33,14 +42,13 @@ public class Main {
 			prix[i]=0;
 			//indice commande
 			for(int j=0;j<tabCommande.length;j++){
-			if(tabClient[i].getNom() == tabCommande[j].getNom()){
+			if(tabClient[i].getNom().equals(tabCommande[j].getNom())){
 				//indice plats
 				for(int x = 0; x<tabPlat.length;x++){
-					if(tabPlat[x].getPlat() == tabCommande[j].getPlat()){
-						prix[i]= (tabPlat[x].getPrix() * tabCommande[j].getQuantite());
+					if(tabPlat[x].getPlat().equals( tabCommande[j].getPlat())){
+						prix[i]+= (tabPlat[x].getPrix() * tabCommande[j].getQuantite());
 					}
 				}
-				break;
 			}
 			}
 			System.out.println(tabClient[i].getNom() +" : "+prix[i]);
@@ -49,59 +57,50 @@ public class Main {
 		
 	}
 
-	public static boolean Verifier(String[] texte){
+	public static int Verifier(String[] texte){
 	int indiceTexte=0;
 	int indiceTab=0;
-	String[] plat;
-	
-	boolean test=true;
-		if(!texte[indiceTexte++].equals("Clients :")){
-			test=false;	
-		}else{
+	String[] plat;	
+	int test=0;
+			indiceTexte++;
 			while(!texte[indiceTexte].equals("Plats :")){
 			tabClient[indiceTab++].setNom(texte[indiceTexte++]);
 	}
 		indiceTab = 0;
+		indiceTexte++;
 		while(!texte[indiceTexte].equals("Commandes :")){ //Verifier le String
 			plat = texte[indiceTexte++].split(" ");
-			tabPlat[indiceTab].setPlat(plat[0]);
-			tabPlat[indiceTab].setPrix(Double.parseDouble(plat[1]));
+			tabPlat[indiceTab].setPlat(plat[0]);			
+			tabPlat[indiceTab++].setPrix(Double.parseDouble(plat[1]));
 	}
 		indiceTab = 0;
+		indiceTexte++;
 		while(!texte[indiceTexte].equals("Fin")){
 			plat=texte[indiceTexte++].split(" ");
-			if(plat.length!=3){
+			if(plat.length==3){
 			for(int i=0;i<tabClient.length;i++ ){
-				if(plat[0] == tabClient[i].getNom()){
-					for(int j=0;j<tabPlat.length;i++ ){
-						if(plat[1] == tabPlat[j].getPlat()){
-						
+				if(plat[0].equals(tabClient[i].getNom())){
+					for(int j=0;j<tabPlat.length;j++ ){
+						if(plat[1].equals(tabPlat[j].getPlat())){
 							try{
 							tabCommande[indiceTab].setNom(plat[0]);
 							tabCommande[indiceTab].setPlat(plat[1]);
 							tabCommande[indiceTab++].setQuantite(Integer.parseInt(plat[2]));
-							test=true;
-							break;
+							test++;
 							}catch(Exception e){
 							System.out.println("Pas une valeur Int possible");
-							break;
 							}
 						}else{
-							test=false;
 						}
 						
 					}
-					break;
 					}else{
-						test=false;
 					}
 					
 				}
 			}else{
-				test=false;
 			}
 			}
-		}
 		return test;
 }
 	}
