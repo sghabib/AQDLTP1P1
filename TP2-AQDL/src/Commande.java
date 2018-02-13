3298b2d61154f2e728b4f452b2a67d3b9e5a07d8
Frederik sylvain <frederik.sylvain6@hotmail.com>
2018-02-13 14:21:21 -0500

    Ajouter Objet prise 3

---
+++ TP2-AQDL/src/Commande.java
@@ -0,0 +1,27 @@
+
+public class Commande {
+
+	public String Nom;
+	public String Plat;
+	public int Quantite;
+	
+	public String getNom() {
+		return Nom;
+	}
+	public void setNom(String nom) {
+		Nom = nom;
+	}
+	public String getPlat() {
+		return Plat;
+	}
+	public void setPlat(String plat) {
+		Plat = plat;
+	}
+	public int getQuantite() {
+		return Quantite;
+	}
+	public void setQuantite(int quantite) {
+		Quantite = quantite;
+	}
+	
+}
---
+++ TP2-AQDL/src/Plats.java
@@ -0,0 +1,20 @@
+
+public class Plats {
+
+	public String Plat;
+	public double Prix;
+	
+	
+	public String getPlat() {
+		return Plat;
+	}
+	public void setPlat(String plat) {
+		Plat = plat;
+	}
+	public double getPrix() {
+		return Prix;
+	}
+	public void setPrix(double prix) {
+		Prix = prix;
+	}
+}
