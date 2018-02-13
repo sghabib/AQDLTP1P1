3b1d83220507a5b480e5593ea1732075089d6843
Remy Brin-Fortier <remy_b-f@hotmail.com>
2018-02-13 14:01:14 -0500

    Fonction lecture completée

--- TP2-AQDL/src/Main.java
+++ TP2-AQDL/src/Main.java
@@ -1,8 +1,12 @@
+import java.io.IOException;
+
+import outilsjava.OutilsLecture;
+
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		new Lecture();
 
 	}
 
